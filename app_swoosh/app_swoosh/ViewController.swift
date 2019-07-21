8ec51748ad491657863ebac1a2b4bd74c9170cbb
Jeffrey Newman <jeffreynewman@jeffreys-mbp.attlocal.net>
2019-07-21 12:47:12 -0400

    first commit

---
+++ app_swoosh/app_swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app_swoosh
+//
+//  Created by Jeffrey Newman on 7/21/19.
+//  Copyright © 2019 jeff.newman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
